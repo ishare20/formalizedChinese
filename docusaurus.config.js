8e530edf271b9bf88fd9f12c90eeb8f6014071b2
lemonTree <745926289@qq.com>
2023-05-13 13:03:39 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ishare20.net',
+  // url: 'https://ishare20.net',
+  url: 'https://ishare20.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/fc/',
+  baseUrl: '/formalizedChinese/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
